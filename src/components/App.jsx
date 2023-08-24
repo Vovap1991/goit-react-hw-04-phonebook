240fc8b81debb9828bd7af6d726e192a67386c25
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-08-24 13:53:08 +0300

    Нові контакти додаються до списку і видаляються

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,20 @@
-import React, { Component } from 'react';
-
 import { ContactForm } from './Form/Form';
 import { Application, FormTitle } from './App.styled';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts !== null) {
-      this.setState({
-        contacts: JSON.parse(savedContacts),
-      });
-    }
-  }
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = newContact => {
-    const { contacts } = this.state;
-
+  const addContact = newContact => {
     const isContactExists = contacts.find(
       contact =>
         contact.name.toLowerCase() === newContact.name.toLowerCase() ||
@@ -44,51 +25,40 @@ export class App extends Component {
       return alert(`${newContact.name} is already in your phonebook`);
     }
 
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, newContact],
-      };
-    });
+    setContacts(prevState => [...prevState, newContact]);
   };
 
-  changeContactFilter = newFilter => {
-    this.setState({ filter: newFilter });
+  const changeContactFilter = newFilter => {
+    setFilter(newFilter);
   };
 
-  handleDelete = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(
-          contact => contact.id !== contactId
-        ),
-      };
-    });
+  const handleDelete = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
   };
 
-  render() {
-    const { filter, contacts } = this.state;
-
-    const visibleContacts = contacts.filter(contact =>
+  const getVisibleContacts = () => {
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
+  };
 
-    return (
-      <div>
-        <Application>
-          <div>
-            <FormTitle>Phonebook</FormTitle>
-            <ContactForm onAddContact={this.addContact}></ContactForm>
-          </div>
-          <div>
-            <FormTitle>Contacts</FormTitle>
-            <Filter value={filter} onChange={this.changeContactFilter} />
-            <ContactList
-              contacts={visibleContacts}
-              onDelete={this.handleDelete}
-            />
-          </div>
-        </Application>
-      </div>
-    );
-  }
-}
+  const visibleContacts = getVisibleContacts();
+
+  return (
+    <div>
+      <Application>
+        <div>
+          <FormTitle>Phonebook</FormTitle>
+          <ContactForm onAddContact={addContact}></ContactForm>
+        </div>
+        <div>
+          <FormTitle>Contacts</FormTitle>
+          <Filter value={filter} onChange={changeContactFilter} />
+          <ContactList contacts={visibleContacts} onDelete={handleDelete} />
+        </div>
+      </Application>
+    </div>
+  );
+};

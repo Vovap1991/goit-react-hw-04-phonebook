63f2856febde2252407b5175a26232c6f2816738
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-08-25 11:06:40 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,32 @@ import { ContactForm } from './Form/Form';
 import { Application, FormTitle } from './App.styled';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+// Зовнішня функція (передана в useState контактів) для отримання данних з local storage. Якщо там нічого немає - то рендер initial contacts
+const getInitialContacts = () => {
+  const savedContacts = localStorage.getItem('contacts');
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
+  }
+  return initialContacts;
+};
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-
+  const [contacts, setContacts] = useState(getInitialContacts);
   const [filter, setFilter] = useState('');
+
+  // Запис контактів у local storage
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = newContact => {
     const isContactExists = contacts.find(
@@ -32,7 +47,7 @@ export const App = () => {
     setFilter(newFilter);
   };
 
-  const handleDelete = contactId => {
+  const DeleteContact = contactId => {
     setContacts(prevState =>
       prevState.filter(contact => contact.id !== contactId)
     );
@@ -56,7 +71,7 @@ export const App = () => {
         <div>
           <FormTitle>Contacts</FormTitle>
           <Filter value={filter} onChange={changeContactFilter} />
-          <ContactList contacts={visibleContacts} onDelete={handleDelete} />
+          <ContactList contacts={visibleContacts} onDelete={DeleteContact} />
         </div>
       </Application>
     </div>
